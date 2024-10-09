9e7f9100f198918565d17dc6e6b3d6393d36b987
sagatorbook <maqa545@gmail.com>
2024-10-08 21:32:49 -0400

    Rotating image based on text angle, Text angle view, Refactoring, README.md updated

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ An Android app built to scan receipts using ML Kit's OCR capabilities. The goal 
   - Store address
   - Receipt date
 
+## Text Recognition and Alignment
+While ML Kit performs well on general text, it struggles with structured, tabular data, such as receipts. Typically, item names and prices are detected as separate text blocks, making it difficult to maintain alignment
+ - Text is captured in a hierarchy of Blocks, Lines, and Elements. Each [Text.Element](https://developers.google.com/android/reference/com/google/mlkit/vision/text/Text.Element) includes its position (`Rect`), which helps reconstruct the layout by placing the elements based on their coordinates
+ - When an image isn’t perfectly aligned (e.g., tilted or rotated), matching item names with their respective prices becomes difficult as they may not appear on the same horizontal line. To fix this, the app first scans the image to determine the average angle of the text `Text.Element.getAngle()`. The source image is then rotated based on this angle
+
 ## Tech Stack
 - **Language:** Kotlin, XML
 - **Libraries:**
--- app/src/main/java/com/example/receiptocr/home/HomeFragment.kt
+++ app/src/main/java/com/example/receiptocr/home/HomeFragment.kt
@@ -17,21 +17,23 @@ import com.example.receiptocr.R
 import com.example.receiptocr.base.BaseFragment
 import com.example.receiptocr.databinding.FragmentHomeBinding
 import com.example.receiptocr.dialogs.ResultDialog
+import com.example.receiptocr.utils.rotate
 import com.google.android.material.snackbar.Snackbar
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.Text.Element
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlin.math.abs
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
-    private val blocksRect = mutableListOf<Rect>() //Rects for displaying lines on output image
+    private val blocksRect = mutableListOf<Rect>() //Rects for displaying elements on output image
     private val linesRect = mutableListOf<Rect>()
     private val elementsRect = mutableListOf<Rect>()
     private val elementsText = mutableListOf<Text.Element>() //All text elements
     private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    private var originalText:String = ""
+    private var originalText: String = ""
     lateinit var cropped: Bitmap
     var rotation: Int = 0
     private lateinit var cropImage: ActivityResultLauncher<CropImageContractOptions>
@@ -65,30 +67,51 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         binding.btnScan.setOnClickListener { // Start OCR if there is an image
             if (::cropped.isInitialized) {
                 showSnackBar("Scanning...", it)
-                val image = InputImage.fromBitmap(cropped, rotation)
-                val result = recognizer.process(image)
+                val srcImage = InputImage.fromBitmap(cropped, rotation)
+                recognizer.process(srcImage)
                     .addOnSuccessListener { visionText ->
-                        processResultText(visionText, it)
-                        showOutputImage()
-                        originalText = visionText.text
+                        val elementsAngle = getElementsForText(visionText).map { el -> el.angle }
+                        val averageAngle = elementsAngle.average() // Getting the average angle of text form the source image
+                        processImage(cropped, averageAngle.toFloat())
                     }
                     .addOnFailureListener { e ->
-                        Toast.makeText(requireContext(), "Error -> ${e.message}", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            requireContext(),
+                            "Error -> ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
             } else {
                 showSnackBar("Add image first", it)
+                return@setOnClickListener
             }
         }
-        binding.btnResult.setOnClickListener {
+
+        binding.btnResult.setOnClickListener { // Show Results Dialog
             ResultDialog(originalText).show(childFragmentManager, ResultDialog.TAG)
         }
-
 
         super.observeView()
     }
 
-    fun processResultText(vText: Text, contextView: View){
-        if(vText.text.isBlank()) {
+    fun processImage(srcBitmap: Bitmap, textAverageAngle: Float) {
+        binding.angle = textAverageAngle.toString()
+        val rotatedBitmap = srcBitmap.rotate(-textAverageAngle) // Rotating source image before getting text
+        cropped = rotatedBitmap
+        val rotatedImage = InputImage.fromBitmap(rotatedBitmap, 0)
+        recognizer.process(rotatedImage)
+            .addOnSuccessListener { visionText ->
+                processResultText(visionText)
+                binding.ivOutImage.setImageBitmap(getOutputImage(cropped, blocksRect, linesRect, elementsRect))
+                originalText = visionText.text
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(requireContext(), "Error -> ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun processResultText(vText: Text) {
+        if (vText.text.isBlank()) {
             Toast.makeText(requireContext(), "No text found, try again", Toast.LENGTH_SHORT).show()
             return
         }
@@ -111,73 +134,86 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         }
         elementsText //Text.Elements List
 
-        binding.tvOutput.text = organizeElements(elementsText).joinToString(separator = "\n") // Displaying organized text
+        binding.tvOutput.text =
+            organizeElements(elementsText).joinToString(separator = "\n") // Displaying organized text
     }
 
-    fun showOutputImage(){
-        val mutableBitmap = cropped.copy(Bitmap.Config.ARGB_8888, true)
-
-        with(Canvas(mutableBitmap)) {  // Show elements on image (output)
-            blocksRect.forEach { drawRect(it, Paint().apply {
-                color = Color.BLUE
-                style = Paint.Style.STROKE
-                strokeWidth = 3f
-            }) }
-            linesRect.forEach { drawRect(it, Paint().apply {
-                color = Color.GREEN
-                style = Paint.Style.STROKE
-                strokeWidth = 3f
-            }) }
-            elementsRect.forEach { drawRect(it, Paint().apply {
-                color = Color.WHITE
-                style = Paint.Style.STROKE
-                strokeWidth = 3f
-            }) }
-        }
-        binding.ivOutImage.setImageBitmap(mutableBitmap)
-    }
-
-    fun organizeElements(elements: List<Text.Element>): List<String> {
+    private fun organizeElements(elements: List<Text.Element>): List<String> {
         // Sort elements first based on their vertical position (top of the bounding box),
         // then based on the horizontal position (left of the bounding box).
         val sortedElements = elements.sortedWith { t1, t2 -> compareEls(t1, t2) }
-
         val lines = mutableListOf<MutableList<Text.Element>>()
 
-        // Group elements into lines
-        for (element in sortedElements) {
+        for (element in sortedElements) { // Group elements into lines
             var foundLine = false
-            // Check if element belongs to an existing line
-            for (line in lines) {
+            for (line in lines) { // Check if element belongs to an existing line
                 if (isElsSameLine(line[0], element)) {
                     line.add(element)
                     foundLine = true
                     break
                 }
             }
-            // If element does not belong to any line, start a new line
-            if (!foundLine) {
+            if (!foundLine) { // If element does not belong to any line, start a new line
                 lines.add(mutableListOf(element))
             }
         }
 
-        // Convert each line of elements into a string with spacing
-        val result = mutableListOf<String>()
-        for (line in lines) {
-            // Sort each line's elements horizontally (left to right)
+        val result = mutableListOf<String>()  // Convert each line of elements into a string with spacing
+        for (line in lines) { // Sort each line's elements horizontally (left to right)
             val sortedLine = line.sortedBy { it.boundingBox!!.left }
-
-            // Combine elements into a single string with only one space between them
-            val lineText = sortedLine.joinToString(" ") { it.text }
-
+            val lineText = sortedLine.joinToString(" ") { it.text } // Combine elements into a single string
             result.add(lineText)
         }
 
         return result
     }
 
+    private fun getOutputImage(bitmap: Bitmap, bRect: List<Rect>, lRect: List<Rect>, eRect: List<Rect>): Bitmap { // Returns output image with blocks, lines and elements
+        val mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
 
-    fun compareEls(t1: Text.Element, t2: Text.Element): Int { // Comparing top and left coordinates to sort elements
+        with(Canvas(mutableBitmap)) {  // Show elements on image (output)
+            bRect.forEach {
+                drawRect(it, Paint().apply {
+                    color = Color.BLUE
+                    style = Paint.Style.STROKE
+                    strokeWidth = 3f
+                })
+            }
+            lRect.forEach {
+                drawRect(it, Paint().apply {
+                    color = Color.GREEN
+                    style = Paint.Style.STROKE
+                    strokeWidth = 3f
+                })
+            }
+            eRect.forEach {
+                drawRect(it, Paint().apply {
+                    color = Color.WHITE
+                    style = Paint.Style.STROKE
+                    strokeWidth = 3f
+                })
+            }
+        }
+
+        return mutableBitmap
+    }
+
+    private fun getElementsForText(text: Text): List<Element> {
+        val mlElements = mutableListOf<Element>()
+        for (block in text.textBlocks) {
+            for (line in block.lines) {
+                for (element in line.elements) {
+                    mlElements.add(element)
+                }
+            }
+        }
+        return mlElements
+    }
+
+    private fun compareEls(
+        t1: Text.Element,
+        t2: Text.Element
+    ): Int { // Comparing top and left coordinates to sort elements
         val diffOfTops = t1.boundingBox!!.top - t2.boundingBox!!.top
         val diffOfLefts = t1.boundingBox!!.left - t2.boundingBox!!.left
 
@@ -203,29 +239,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         return true
     }
 
-    fun showSnackBar(text: String, contextView: View){
+    private fun showSnackBar(text: String, contextView: View) {
         Snackbar.make(contextView, text, 800)
             .show()
 
     }
 }
-
-
-
-//for (block in vText.textBlocks) {
-//    val blockText = block.text
-//    val blockCornerPoints = block.cornerPoints
-//    val blockFrame = block.boundingBox
-//    for (line in block.lines) {
-//        val lineText = line.text
-//        val lineCornerPoints = line.cornerPoints
-//        val lineFrame = line.boundingBox
-//        linesRect.add(line.boundingBox!!)
-//        for (element in line.elements) {
-//            val elementText = element.text
-//            val elementCornerPoints = element.cornerPoints
-//            val elementFrame = element.boundingBox
-//            elementsRect.add(element.boundingBox!!)
-//        }
-//    }
-//}
